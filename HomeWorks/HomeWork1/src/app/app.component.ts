fe33c667b4e70edb262d42c1737aa93f4adf4db2
MikolaLvivUA <mikola.shidlovsky@gmail.com>
2019-10-29 16:04:16 +0200

    HomeWork1 + Create search field + create logIn and register form + Show all houses

---
+++ HomeWorks/HomeWork1/src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'HomeWork1';
+  InputValue: string | number;
+  ShowingValue: string | number;
+  RClick: boolean = false;
+  LClick: boolean = false;
+  ShowingAllHouse: any[] = [];
+
+  housesArray = [
+    {
+      id: 1,
+      city: 'Lviv'
+    },
+    {
+      id: 2,
+      city: 'Kyiv'
+    },
+    {
+      id: 3,
+      city: 'Odesa'
+    },
+    {
+      id: 4,
+      city: 'Dnipro'
+    }
+  ];
+
+
+  onInput(event) {
+    this.InputValue = event.target.value;
+  }
+
+  clickSearchBtn() {
+    this.ShowingValue = this.InputValue;
+  }
+
+  registerClick() {
+    this.RClick = !this.RClick;
+    this.LClick = false;
+  }
+
+  logClick() {
+    this.LClick = !this.LClick;
+    this.RClick = false;
+  }
+
+  ShowAllClick() {
+    this.ShowingAllHouse = this.housesArray;
+  }
+}
